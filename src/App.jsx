e4e2db9fedc273eaebd391416ef712e01cd31c2c
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-10-04 19:14:10 +0200

    feat: integrate redux

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,25 @@
 import './App.css';
 import Home from './pages/home.page.jsx';
 import Products from './pages/product.page.jsx';
+import Cart from './pages/cart.page.jsx';
 import Navbar from './components/navbar.jsx';
-
+import { Provider } from 'react-redux';
+import store from './store.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <BrowserRouter>
-      <Navbar></Navbar>
-      <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/products' element={<Products></Products>}></Route>
-      </Routes>
-    </BrowserRouter>
-  )
+    <Provider store={store}>
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/products' element={<Products />} />
+          <Route path='/cart' element={<Cart />} />
+        </Routes>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
