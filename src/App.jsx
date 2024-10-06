0c7ed06d0350ee5a93978f20400423d8ef97c294
Alina Zakharchenko <alina.zakharchenko03@gmail.com>
2024-10-06 21:31:58 +0200

    feat: add Footer component

--- src/App.jsx
+++ src/App.jsx
@@ -6,19 +6,25 @@ import Header from './components/Header.jsx';
 import { Provider } from 'react-redux';
 import store from './store.jsx';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import ProductPage from './pages/ProductPage.jsx';
+import Footer from './components/Footer.jsx';
 
 function App() {
   return (
-    <Provider store={store}>
-      <BrowserRouter>
-        <Header />
-        <Routes>
-          <Route path='/' element={<Home />} />
-          <Route path='/products' element={<Products />} />
-          <Route path='/cart' element={<Cart />} />
-        </Routes>
-      </BrowserRouter>
-    </Provider>
+    <div className='app-container'>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/products' element={<Products />} />
+            <Route path='/cart' element={<Cart />} />
+            <Route path="/catalog/:id" element={<ProductPage />} />
+          </Routes>
+          <Footer />
+        </BrowserRouter>
+      </Provider>
+    </div>
   );
 }
 
